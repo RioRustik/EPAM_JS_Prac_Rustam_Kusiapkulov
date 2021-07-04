1a30ab305dfebbc30a2c7c8d0b58590b53234bed
DESKTOP-RFP4SCT\Smorodina <Smorodina>
2021-07-04 13:26:51 +0400

    no message

--- src/index.js
+++ src/index.js
@@ -5,31 +5,31 @@ const contentSpeaker = document.querySelector('.contentSpeaker');
 const contentStream = document.querySelector('.contentStream');
 
 let nav = document.querySelectorAll('.barBlock');
-let elem;
+let changeElementsPage;
 
 for (var i = 0; i < nav.length; i++) {
     nav[i].addEventListener('click', navToggle)
 }
 
 function navToggle(event){
-    let target = event.target.closest('button')
-    if (elem) { 
-        elem.classList.remove('backlightitem');
+    let activeButton = event.target.closest('button')
+    if (changeElementsPage) { 
+        changeElementsPage.classList.remove('backlightitem');
     }
-    elem = target;
-    elem.classList.add('backlightitem'); 
+    changeElementsPage = activeButton;
+    changeElementsPage.classList.add('backlightitem'); 
 
-    if(elem.classList.contains('voices')){
+    if(changeElementsPage.classList.contains('voices')){
         contentVoices.classList.add("contentActive");
         contentSpeaker.classList.remove("contentActive");
         contentStream.classList.remove("contentActive");
     }
-    if(elem.classList.contains('speaker')){
+    if(changeElementsPage.classList.contains('speaker')){
         contentSpeaker.classList.add("contentActive");
         contentVoices.classList.remove("contentActive");
         contentStream.classList.remove("contentActive");
     }
-    if(elem.classList.contains('stream')){
+    if(changeElementsPage.classList.contains('stream')){
         contentStream.classList.add("contentActive");
         contentVoices.classList.remove("contentActive");
         contentSpeaker.classList.remove("contentActive");
