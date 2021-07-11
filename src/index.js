73a188500e8840154c29ec42e3b2ec1ed12705ed
DESKTOP-RFP4SCT\Smorodina <Smorodina>
2021-07-11 17:39:39 +0400

    no message

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
+import './script/page-proofs';
+import './script/speaker';
+import './script/stream';
+import './script/voices';
 import './styles/style.scss';
-
-const sectionAll = document.querySelectorAll('section');
-const nav = document.querySelectorAll('.barBlock');
-let changeElementsPage;
-let activeButtonCheck = '';
-let repeatedButtonPress = true;
-
-for (let i = 0; i < nav.length; i++) {
-    nav[i].addEventListener('click', navToggle);
-}
-function navToggle(event){
-    let activeButton = event.target.closest('button');
-
-    if (changeElementsPage) { 
-        changeElementsPage.classList.remove('backlightitem');
-    }
-    changeElementsPage = activeButton;
-    changeElementsPage.classList.add('backlightitem');
-
-    if(activeButtonCheck == activeButton.getAttribute('checkСonnect')){
-        repeatedButtonPress = false;
-    }
-
-    for (let j = 0; j < sectionAll.length; j++) {
-        if((sectionAll[j].classList.contains('contentActive') 
-        || sectionAll[j].getAttribute('checkСonnect') == activeButton.getAttribute('checkСonnect'))
-        && repeatedButtonPress){
-            sectionAll[j].classList.toggle('contentActive');
-        }
-    }
-
-    repeatedButtonPress = true;
-    activeButtonCheck = activeButton.getAttribute('checkСonnect');
-}
